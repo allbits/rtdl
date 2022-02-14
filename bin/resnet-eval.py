959df81c131338927c0fdb67709035cf9f1c7b8f
Marcus Ritt <marcus.ritt@inf.ufrgs.br>
2022-02-14 22:44:22 +0000

    Add a stub for the ResNet evaluator, more cleanup.

--- bin/resnet-eval.py
+++ bin/resnet-eval.py
@@ -2,27 +2,27 @@
 import zero
 import lib
 
-from ft_transformer import *
+from resnet import *
 import evaluation
 
 # %%
 if __name__ == "__main__":
     ## (1) load the configuration, general setup
     args, output = lib.load_config()
-    args['model'].setdefault('token_bias', True)
-    args['model'].setdefault('kv_compression', None)
-    args['model'].setdefault('kv_compression_sharing', None)
+
+    dataset_dir = lib.get_path(args['data']['path'])
+    dataset_info = lib.load_json(dataset_dir / 'info.json')
 
     seed=args['seed']
     zero.set_randomness(seed)
 
-    ## (2) load the transformation data, get example data
+    ## (2) load the transformation data
     tf = evaluation.Normalization(args,seed)
-
+    
     ## (3) load the model (and possibly move it to the GPU)
     print(f'\nLoading model ({tf.n_num_features}n/{tf.n_cat_features}c) ...')
     device = lib.get_device()
-    model = Transformer(d_numerical=tf.n_num_features, categories=tf.cat_values,
+    model = ResNet(d_numerical=tf.n_num_features, categories=tf.cat_values,
         d_out=1, ## regression hardcoded
         **args['model'],
     ).to(device)
